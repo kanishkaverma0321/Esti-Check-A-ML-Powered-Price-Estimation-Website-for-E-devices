ce3256667eebd54f990ba63253aca8b871d812bf
kanishkaverma0321 <kanishkas.22.becs@acharya.ac.in>
2025-08-07 19:28:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Esti-Check-A-ML-Powered-Price-Estimation-Website-for-E-devices/',  // 👈 Add this
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
